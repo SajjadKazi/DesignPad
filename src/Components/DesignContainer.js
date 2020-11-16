181556781ebf74a1fe60c3d00752965570e8a40a
Sajjad Kazi <sajjadkazi4@gmail.com>
2020-11-16 10:48:16 -0500

    adding proj files

---
+++ src/Components/DesignContainer.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import PadInput from './PadInput';
+
+
+class DesignContainer extends Component {
+   constructor(props){
+       super(props)
+   }
+
+
+render() {
+   return (
+
+<div>
+
+        <PadInput/>
+
+
+</div>
+
+   );
+}
+
+}
+export default DesignContainer;
---
+++ src/Components/PadInput.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './PadInput.css';
+
+
+class PadInput extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+}
+
+
+
+export default PadInput
